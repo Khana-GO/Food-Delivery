18357f1ace90a6d048f92c21d2265a57eb340947
Shushilbhusal <sbhusal281@gmail.com>
2026-07-16 21:54:01 +0545

    frontend setup

---
+++ apps/mobile/lib/token-cache.ts
@@ -0,0 +1,12 @@
+import { TokenCache } from '@clerk/expo';
+import * as SecureStore from 'expo-secure-store';
+
+export const tokenCache: TokenCache = {
+  async getToken(key: string) {
+    return SecureStore.getItemAsync(key);
+  },
+
+  async saveToken(key: string, value: string) {
+    return SecureStore.setItemAsync(key, value);
+  },
+};
