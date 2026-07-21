205ab4bfa5438caa0aaf6db76223fdae608d645c
Shushilbhusal <sbhusal281@gmail.com>
2026-07-21 22:09:35 +0545

    auth changes

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { MailModule } from '../mail/mail.module';
 import { AuthController } from './controllers/auth.controller';
-import { ProtectedController } from './controllers/protected.controller';
 import { RefreshController } from './controllers/refresh.controller';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { RolesGuard } from './guards/roles.guard';
@@ -15,7 +14,7 @@ import { SessionsModule } from '../sessions/session.module';
     MailModule,
     SessionsModule
   ],
-  controllers: [AuthController, RefreshController, ProtectedController],
+  controllers: [AuthController, RefreshController],
   providers: [AuthService, JwtAuthGuard, RolesGuard],
   exports: [AuthService, JwtAuthGuard, RolesGuard],
 })
